ceadf27b76c8ba4fedb605372c362c77ac7f574a
VictorDG00 <victord.goncalves@outlook.com>
2021-03-01 22:23:07 -0300

    aula1

---
+++ Bloco_4/dia_1/scripts.js
@@ -0,0 +1,8 @@
+let idade = 20;
+let nome = "Hulk";
+let birthCity = "Guarulhos";
+let birthYear = 2020;
+
+
+console.log(birthCity);
+console.log(birthYear);
---
+++ Bloco_4/dia_1/scripts2.js
@@ -0,0 +1,33 @@
+// let movie = "Ave0ngers"; //string literal
+// let score = 10; //number literal
+// let isValid = true; //boolean
+// let name; //undefined
+// let color = null;  //nula, e é só isso mesmo
+
+// //+, -, *, /, **
+// //++, --  x + 1 = x1, x - 1 = x-1 
+
+// let salary = 3500;
+
+// console.log(salary + salary);
+// console.log(salary - salary);
+// console.log(salary * salary);
+// console.log(salary / salary);
+// console.log(3 ** 3);
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetros = base * 2 + altura * 2;
+
+console.log(base);
+console.log(altura);
+console.log(area);
+console.log(perimetros);
