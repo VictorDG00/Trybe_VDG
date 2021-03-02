8b845bd7119887d99614c8a177bbec353f485188
VictorDG00 <victord.goncalves@outlook.com>
2021-03-02 16:45:07 -0300

    inicio exercicios

---
+++ Bloco_4/dia_1/scripts3.js
@@ -0,0 +1,40 @@
+// if - se 
+//else - senão
+//else if - se senão
+
+// if (condição){
+//   //codigo
+// }
+// else if (condição){
+//   //outra condição
+// }
+// else {
+//   //condiçao exessiva
+// }
+// let trybe = 16.40;
+
+// if (trybe >= 14 && trybe < 14.40 ){
+//   console.log("Esquenta");  
+// }
+// else if (trybe >= 16.30 && trybe < 17.50){
+//   console.log("Aula ao vivo");
+// }
+// else if (trybe >= 19.40 && trybe < 20){
+//   console.log("Fechamento")
+// }
+// else {
+//   console.log("Fora dos momentos sincronos")
+// }
+let nota = 100;
+if (nota >= 80){
+  console.log("Parabéns, você foi aprovada(o)!");
+  //codigo
+}
+else if (nota >= 60){
+  console.log("Você está na nossa lista de espera");
+  //outra condição
+}
+else {
+  console.log("Você foi reprovada(o)")
+  //condiçao exessiva
+}
---
+++ Bloco_4/dia_1/scripts4.js
@@ -0,0 +1,36 @@
+//switch -- case
+
+// let trafficLight = "potato";
+
+// switch (trafficLight){
+//   case "vermelho":
+//     console.log("Pare");
+//     break;
+  
+//     case "amarelo":
+//     console.log("Atenção");
+//     break;
+
+//   case "verde":
+//     console.log("Verde");
+//     break;
+
+//   default:
+//     console.log("Valor não identificado")
+// }
+
+let pessoaCandidata = "aprovada";
+
+switch (pessoaCandidata){
+  case "aprovada":
+    console.log("aprovada");
+    break;
+  case "lista":
+    console.log("lista");
+    break;
+  case "reprovada":
+    console.log("reprovada");
+    break;
+  default:
+    console.log("não se aplica")
+}
