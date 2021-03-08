4eaafeff4d71a7a8ef5dfe7e7f9592eeddd60772
VictorDG00 <victord.goncalves@outlook.com>
2021-03-08 15:01:01 -0300

    fim do primeiro dia

---
+++ Bloco_4/dia_2/Aula/scripts1.js
@@ -0,0 +1,32 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// console.log(tasksList.length);
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+// console.log(tasksList);
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+// // imprime o primeiro termo
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+// // mostra o a posição 0 - 1, que resulta em -1  que tb é a ultima posição do array
+
+// tasksList.pop();  // remove a última tarefa
+// tasksList.shift() // remove o primeiro
+// console.log(tasksList);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+let indexOfPortfolio = menu[2];
+
+console.log(indexOfPortfolio);
+
+menu.push('Contato')
+
+console.log(menu);
