b702004f5baa78121ce0add37af8a50a4a552305
VictorDG00 <victord.goncalves@outlook.com>
2021-03-08 19:38:01 -0300

    ex 4 e 5 feitos

--- Bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/Exercicio4.js
+++ Bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/Exercicio4.js
@@ -1,1 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let c = 0; c < numbers.length; c++){
+  soma = soma + numbers[c];
+}
+soma = soma / (numbers.length);
+
+if (soma > 20){
+  console.log("Valor maior que 20");
+}
+else{
+  console.log("Valor menor que 20");
+}
