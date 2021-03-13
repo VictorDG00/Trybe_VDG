4af6541308a6f545b82f10564783039fd074d39e
VictorDG00 <victord.goncalves@outlook.com>
2021-03-13 19:56:53 -0300

    fim do bloco4

---
+++ Bloco_4/dia_4/Aula/scripts2.js
@@ -0,0 +1,14 @@
+// let car = {
+//   type: "Fiat",
+//   model: "500",
+//   color: "white",
+// };
+
+// for (let i in car) {
+//   console.log(i, car[i]);
+// }
+
+var fruits = ["Apple", "Banana", "Orange"];
+for (x of fruits){
+  console.log(x);
+}
